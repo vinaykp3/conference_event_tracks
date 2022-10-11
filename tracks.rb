24f08dad11e2e1e79a9b8c477eb56a58983c7592
vinay-wi <vinay.kp@weinvest.net>
2022-10-11 05:50:38 +0530

    changes

--- tracks.rb
+++ tracks.rb
@@ -13,32 +13,63 @@ proposal_time_categories = {
 "5"=>["Rails for Python Developers"]
 }
 sessions = [
-[60, "Writing Fast Tests Against Enterprise Rails 60min"],[45, "Overdoing it in Python 45min"], [30, "Lua for the Masses 30min"], [45, "Ruby Errors from Mismatched Gem Versions 45min"], [45,"Common Ruby Errors 45min"], [5, "Rails for Python Developers lightning"], [60, "Communicating Over Distance 60min"],[45, "Accounting-Driven Development 45min"], [30, "Woah 30min"], [30, "Sit Down and Write 30min"], [45, "Pair Programming vs Noise 45min"],[60, "Rails Magic 60min"], [60, "Ruby on Rails: Why We Should Move On 60min"], [45, "Clojure Ate Scala (on my project) 45min"], [30, "Programming in the Boondocks of Seattle 30min"], [30, "Ruby vs. Clojure for Back-End Development 30min"], [60, "Ruby on Rails Legacy App Maintenance 60min"],[30, "A World Without HackerNews 30min"], [30, "User Interface CSS in Rails Apps 30min"]
+[60, "Writing Fast Tests Against Enterprise Rails 60min"],
+[45, "Overdoing it in Python 45min"], 
+[30, "Lua for the Masses 30min"], 
+[45, "Ruby Errors from Mismatched Gem Versions 45min"], 
+[45,"Common Ruby Errors 45min"], 
+[5, "Rails for Python Developers lightning"], 
+[60, "Communicating Over Distance 60min"],
+[45, "Accounting-Driven Development 45min"], 
+[30, "Woah 30min"], 
+[30, "Sit Down and Write 30min"], 
+[45, "Pair Programming vs Noise 45min"],
+[60, "Rails Magic 60min"], 
+[60, "Ruby on Rails: Why We Should Move On 60min"], 
+[45, "Clojure Ate Scala (on my project) 45min"], 
+[30, "Programming in the Boondocks of Seattle 30min"], 
+[30, "Ruby vs. Clojure for Back-End Development 30min"], 
+[60, "Ruby on Rails Legacy App Maintenance 60min"],
+[30, "A World Without HackerNews 30min"], [30, "User Interface CSS in Rails Apps 30min"]
 ]
+
 sessions = sessions.sort.reverse
 lunch_start = 180
+noon_start_time = 240
 sessions_end_minutes = 480
 start_time = Time.new(2022, 7, 8, 9, 00)
 i = 1
+index_value = 0
 tracks = [[], []]
 tracks.each do |track|
 	begin_time = 0
-	index = 0
 	time = start_time
-	while(sessions.length != 0)
-		if begin_time == lunch_start
-			track << "#{time.strftime('%I:%M %p')}" + " LUNCHTIME"
-			time = time + 60*60
-			begin_time = begin_time + 60
-		elsif begin_time >= sessions_end_minutes
-			track << "#{time.strftime('%I:%M %p')}" + " NetWorkingEvent"
+	while(index_value <= sessions.length - 1)
+		begin
+			element = sessions[index_value]
+			new_time = begin_time + element[0]
+			# pp "#{new_time} and index #{index_value} and element #{element}"
+			if new_time <= 180
+				begin_time = begin_time + element[0]
+				track << "#{time.strftime('%I:%M %p')} " + element[1]
+				time = time + (element[0]*60)
+				index_value = index_value + 1
+			elsif new_time > 240 && new_time <= sessions_end_minutes
+				begin_time = begin_time + element[0]
+				track << "#{time.strftime('%I:%M %p')} " + element[1]
+				time = time + (element[0]*60)
+				index_value = index_value + 1
+			elsif new_time > 180 && new_time <= 240
+				track << "#{time.strftime('%I:%M %p')}" + " LUNCHTIME"
+				time = time + 60*60
+				begin_time = begin_time + 60
+			elsif new_time > sessions_end_minutes
+				track << "#{time.strftime('%I:%M %p')}" + " NetWorkingEvent"
+				break
+			end
+		rescue=>e
+			pp "nothing"	
 			break
-		elsif begin_time != lunch_start && begin_time < sessions_end_minutes
-			element = sessions.shift()
-			track << "#{time.strftime('%I:%M %p')} " + element[1]
-			index = index + 1
-			time = time + (element[0]*60)
-			begin_time = begin_time + element[0]
 		end
 	end
 	pp "Track#{i}"
